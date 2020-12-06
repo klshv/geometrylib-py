bc1131986a96b98dcaf7a493e8063193e1592128
Елизавета Кулешова <elizabeth.kuleshova@gmail.com>
2020-12-06 20:23:03 +0700

    убрали лишнее

--- main.py
+++ main.py
@@ -4,11 +4,3 @@ from triangle import Triangle2D
 
 t = Triangle2D(Point2D(1,3), Point2D(3,7), Point2D(6,9))
 print(t.getPerimeter())
-
-p1 = Point3D(5, 10, 10)
-print(p1)
-
-l = Line(p1, Point3D(1,2,34))
-print(l.start)
-print(l.end)
-print(l.getLength())
